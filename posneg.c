228be20aa39abeea160618284c02c3bd581e8495
arunrajaram <arundhileep619@gmail.com>
2016-12-26 20:20:33 -0800

    Add files via upload

---
+++ posneg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,value,b;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    if(a==0)
+        printf("It is zero");
+    else{
+        value=(a>0?printf("positive"):printf("negative"));
+
+}
+}
