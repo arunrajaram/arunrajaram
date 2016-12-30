2b10a3505b9b0fcaed2053c946e73ce283615986
arunrajaram <arundhileep619@gmail.com>
2016-12-29 19:54:21 -0800

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("\n Enter the number:");
+    scanf("%d",&a);
+    b=(a%2==0?printf("\n %d is even number",a):printf("\n %d is odd number",a));
+    }
